aab45ee8589d959c23ded77e4e0dab4778138f37
Kasım Yüksel <kasimyueksel@gmail.com>
2020-11-15 23:54:09 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+export default class DrawerMenu extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text style={{color:'red'}}>Test Component</Text>
+            </View>
+        )
+    }
+}
